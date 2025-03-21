311abc74bfe07a0a54386a49c9558b0a5e0378f5
Alean Mihaylov <minefillgreat@gmail.com>
2025-03-21 21:18:57 +0200

    V1.2 Project1Web

---
+++ Sign Up/SignUp.js
@@ -0,0 +1,27 @@
+// const registerForm = document.getElementById("registerForm");
+// registerForm.addEventListener("submit", SignUp);
+// function SignUp(event) {
+//     event.preventDefault();
+//     const form = event.target;
+//     const formData = new FormData(form);
+// };
+// const username = FormData.get("username");
+// const emil = FormData.get("email");
+// const pssword = FormData.get("password");
+
+// document.getElementById("output").innerText = `username: ${username}, email ${email}`;
+// password = "123"
+//     switch(password) {
+//         case "123":
+//             console.log("password is correct");
+//         break;
+// };
+
+
+
+// STUDY/
+
+
+    
+    
+    
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+//More Button
+const more = document.getElementById("more");
+    more.addEventListener("mouseover", function () {
+    more.style.transform = "scale(1.1)";
+    more.style.transition = "transform 0.5s ease";
+});
+const more2 = document.getElementById("more");
+more2.addEventListener("mouseout", function () {
+    more2.style.transform = "scale(1)";
+});
 
+
+
+//More Button/
